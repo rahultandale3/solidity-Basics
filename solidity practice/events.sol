2c28025b8a79f88982ab36a1c3e0319595995f39
rahultandale3 <techicalguys9512@gmail.com>
2024-09-23 21:11:34 +0530

    events handler in solidity

---
+++ solidity practice/events.sol
@@ -0,0 +1,19 @@
+
+// My First Smart Contract
+// SPDX-License-Identifier: MIT 
+pragma solidity >=0.5.0 <0.9.0;
+
+contract eventExample {
+
+	// Declaring state variables
+	uint256 public value = 0;
+
+	// Declaring an event
+	event Increment(address owner); 
+
+	// Defining a function for logging event 
+	function getValue(uint _a, uint _b) public {
+		emit Increment(msg.sender); 
+		value = _a + _b;
+	}
+}
